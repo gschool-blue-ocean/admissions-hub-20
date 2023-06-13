1675de3a92431c1e19e7757ec1dd032be64afc4b
Chris Stewart <Christopher.j.Stewart93@gmail.com>
2023-06-13 17:51:15 -0500

    Removed login/signup elements from sidebar

--- client/src/components/SidebarNav/SidebarNav.jsx
+++ client/src/components/SidebarNav/SidebarNav.jsx
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 const SidebarNav = () => {
   return (
     <nav className="p-4">
-        <div className="d-flex justify-content-center align-items-center">
-            <img src="../assets/GSymbol.webp" alt="Galvanizze Inc logo" width={"48px"} />
-            <div className="ms-1">
-                <p className="m-0 fs-5">Galvanize</p>
-                <p className="m-0 fs-5 text-secondary">Admissions</p>
-            </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <img
+          src="../assets/GSymbol.webp"
+          alt="Galvanizze Inc logo"
+          width={"48px"}
+        />
+        <div className="ms-1">
+          <p className="m-0 fs-5">Galvanize</p>
+          <p className="m-0 fs-5 text-secondary">Admissions</p>
         </div>
+      </div>
       <ul className="list-unstyled pt-5">
         <li className="text-secondary pb-3">MAIN MENU</li>
         <li>
@@ -21,12 +25,6 @@ const SidebarNav = () => {
         </li>
         <li>
           <Link to="/editprofile">EDIT PROFILE</Link>
-        </li>
-        <li>
-          <Link to="/login">LOGIN</Link>
-        </li>
-        <li>
-          <Link to="/signup">SIGN UP</Link>
         </li>
       </ul>
     </nav>
